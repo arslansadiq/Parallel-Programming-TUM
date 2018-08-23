11e23d6f85af0c2fcbaf0b7a7c89dd9a8dffe8df
Muhammad Arslan Ansari <m.arslansadiq@yahoo.com>
2018-08-23 15:34:21 +0200

    Add files via upload

---
+++ assignment4/student/main_par.d
@@ -0,0 +1,10 @@
+student/main_par.o: main.cpp x_gradient.h x_gradient_ref.h \
+ student/x_gradient_par.h /home/arslan/Desktop/assignment4/x_gradient.h
+
+x_gradient.h:
+
+x_gradient_ref.h:
+
+student/x_gradient_par.h:
+
+/home/arslan/Desktop/assignment4/x_gradient.h:
