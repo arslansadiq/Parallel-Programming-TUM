b9df199909d2e95798bb582ec67e9d2df9dd1bca
Muhammad Arslan Ansari <m.arslansadiq@yahoo.com>
2018-08-24 01:26:39 +0200

    Add files via upload

---
+++ Assignment2_Pthread2/student/mandelbrot_set_par.d
@@ -0,0 +1,4 @@
+student/mandelbrot_set_par.o: student/mandelbrot_set_par.c \
+ /home/arslan/Desktop/assignment2/mandelbrot_set.h
+
+/home/arslan/Desktop/assignment2/mandelbrot_set.h:
---
+++ Assignment2_Pthread2/student/mandelbrot_set_seq.d
@@ -0,0 +1,3 @@
+student/mandelbrot_set_seq.o: mandelbrot_set_seq.c mandelbrot_set.h
+
+mandelbrot_set.h:
