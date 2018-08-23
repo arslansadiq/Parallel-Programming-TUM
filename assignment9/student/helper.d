5b4f90703b0dd25349c11945afbd5c00cbc6750e
Muhammad Arslan Ansari <m.arslansadiq@yahoo.com>
2018-08-23 22:41:07 +0200

    Add files via upload

---
+++ assignment9/student/helper.d
@@ -0,0 +1,1 @@
+student/helper.o: helper.c
---
+++ assignment9/student/unit_test.d
@@ -0,0 +1,10 @@
+student/unit_test.o: unit_test.c /usr/lib/openmpi/include/mpi.h \
+ /usr/lib/openmpi/include/mpi_portable_platform.h helper.h reverse.h
+
+/usr/lib/openmpi/include/mpi.h:
+
+/usr/lib/openmpi/include/mpi_portable_platform.h:
+
+helper.h:
+
+reverse.h:
