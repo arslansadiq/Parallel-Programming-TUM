674dfdc82f0763d816dafd5e4c27925506274a57
Muhammad Arslan Ansari <m.arslansadiq@yahoo.com>
2018-08-23 22:47:01 +0200

    Add files via upload

---
+++ assignment10/student/heat.d
@@ -0,0 +1,8 @@
+student/heat.o: heat.c heat.h /usr/lib/openmpi/include/mpi.h \
+ /usr/lib/openmpi/include/mpi_portable_platform.h
+
+heat.h:
+
+/usr/lib/openmpi/include/mpi.h:
+
+/usr/lib/openmpi/include/mpi_portable_platform.h:
---
+++ assignment10/student/heat_par.d
@@ -0,0 +1,15 @@
+student/heat_par.o: student/heat_par.c /usr/lib/openmpi/include/mpi.h \
+ /usr/lib/openmpi/include/mpi_portable_platform.h \
+ /home/arslan/Desktop/assignment10/heat.h \
+ /home/arslan/Desktop/assignment10/helper.h \
+ /home/arslan/Desktop/assignment10/heat.h
+
+/usr/lib/openmpi/include/mpi.h:
+
+/usr/lib/openmpi/include/mpi_portable_platform.h:
+
+/home/arslan/Desktop/assignment10/heat.h:
+
+/home/arslan/Desktop/assignment10/helper.h:
+
+/home/arslan/Desktop/assignment10/heat.h:
