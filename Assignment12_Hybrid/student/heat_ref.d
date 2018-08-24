ed67d22ab01f6cfe4a4bfb601107fdf0dd7ed531
Muhammad Arslan Ansari <m.arslansadiq@yahoo.com>
2018-08-24 15:27:22 +0200

    Add files via upload

---
+++ Assignment12_Hybrid/student/heat_ref.d
@@ -0,0 +1,10 @@
+student/heat_ref.o: heat_ref.c /usr/lib/openmpi/include/mpi.h \
+ /usr/lib/openmpi/include/mpi_portable_platform.h heat.h helper.h
+
+/usr/lib/openmpi/include/mpi.h:
+
+/usr/lib/openmpi/include/mpi_portable_platform.h:
+
+heat.h:
+
+helper.h:
---
+++ Assignment12_Hybrid/student/heat_seq.d
@@ -0,0 +1,10 @@
+student/heat_seq.o: heat_seq.c /usr/lib/openmpi/include/mpi.h \
+ /usr/lib/openmpi/include/mpi_portable_platform.h heat.h helper.h
+
+/usr/lib/openmpi/include/mpi.h:
+
+/usr/lib/openmpi/include/mpi_portable_platform.h:
+
+heat.h:
+
+helper.h:
