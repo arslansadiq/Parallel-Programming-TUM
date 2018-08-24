a2b29ff4ae8ff9713818ae3d1fdc37f3b5bf6c95
Muhammad Arslan Ansari <m.arslansadiq@yahoo.com>
2018-08-24 21:47:48 +0200

    Add files via upload

---
+++ Assignment11_mpiP/CoMD_info.h
@@ -0,0 +1,15 @@
+#ifndef CoMD_info_hpp
+#define CoMD_info_hpp
+
+#define CoMD_VARIANT "CoMD-mpi"
+#define CoMD_HOSTNAME "arslan-VirtualBox"
+#define CoMD_KERNEL_NAME "'Linux'"
+#define CoMD_KERNEL_RELEASE "'4.13.0-36-generic'"
+#define CoMD_PROCESSOR "'x86_64'"
+
+#define CoMD_COMPILER "'/usr/bin/mpicc'"
+#define CoMD_COMPILER_VERSION "'gcc (Ubuntu 5.4.0-6ubuntu1~16.04.10) 5.4.0 20160609'"
+#define CoMD_CFLAGS "'-std=c99 -DDOUBLE -DDO_MPI -g -O5   '"
+#define CoMD_LDFLAGS "'-lrt -I /home/arslan/Desktop/assignment11 -L /home/arslan/mpiP -lmpiP -ldl -lm -lunwind  -lm '"
+
+#endif
