64fe49965130cf904a96ca6803d2b5c47b47e17f
Muhammad Arslan Ansari <m.arslansadiq@yahoo.com>
2018-08-24 13:23:22 +0200

    Add files via upload

---
+++ Assignment4_Openmp_1/student/unit_test.d
@@ -0,0 +1,10 @@
+student/unit_test.o: unit_test.cpp x_gradient.h x_gradient_ref.h \
+ student/x_gradient_par.h /home/arslan/Desktop/assignment4/x_gradient.h
+
+x_gradient.h:
+
+x_gradient_ref.h:
+
+student/x_gradient_par.h:
+
+/home/arslan/Desktop/assignment4/x_gradient.h:
