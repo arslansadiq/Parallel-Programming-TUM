8a464caea1b7d36a934b0848b6c381e4f76a9b72
Muhammad Arslan Ansari <m.arslansadiq@yahoo.com>
2018-08-24 15:24:04 +0200

    Add files via upload

---
+++ Aassignment10_MPI_non_blocking/student/main.d
@@ -0,0 +1,10 @@
+student/main.o: main.c /usr/lib/openmpi/include/mpi.h \
+ /usr/lib/openmpi/include/mpi_portable_platform.h heat.h helper.h
+
+/usr/lib/openmpi/include/mpi.h:
+
+/usr/lib/openmpi/include/mpi_portable_platform.h:
+
+heat.h:
+
+helper.h:
