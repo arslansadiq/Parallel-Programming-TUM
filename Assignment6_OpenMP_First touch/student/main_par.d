069dcac0672106f370445f004eb44decdb327192
Muhammad Arslan Ansari <m.arslansadiq@yahoo.com>
2018-08-24 15:06:42 +0200

    Add files via upload

---
+++ Assignment6_OpenMP_First touch/student/main_par.d
@@ -0,0 +1,8 @@
+student/main_par.o: main.cpp laplace.h laplace_ref.h \
+ student/laplace_par.h
+
+laplace.h:
+
+laplace_ref.h:
+
+student/laplace_par.h:
---
+++ Assignment6_OpenMP_First touch/student/unit_test.d
@@ -0,0 +1,8 @@
+student/unit_test.o: unit_test.cpp laplace.h laplace_ref.h \
+ student/laplace_par.h
+
+laplace.h:
+
+laplace_ref.h:
+
+student/laplace_par.h:
