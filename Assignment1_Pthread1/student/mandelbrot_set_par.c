0680436ffe173c7ddbc113cc31f1a0a48c229cc4
Muhammad Arslan Ansari <m.arslansadiq@yahoo.com>
2019-05-15 19:11:15 +0200

    Update mandelbrot_set_par.c

--- Assignment1_Pthread1/student/mandelbrot_set_par.c
+++ Assignment1_Pthread1/student/mandelbrot_set_par.c
@@ -4,80 +4,50 @@
 #include <string.h>
 #include <complex.h>
 #include <pthread.h>
-#include <omp.h>
+
 #include "mandelbrot_set.h"
 
-struct thread_obj{
-int x_resolution, y_resolution, max_iter, paletter_shift, num_threads, thread_num;
-double view_x0, view_x1, view_y0, view_y1, x_stepsize, y_stepsize;
-void *image;
+struct pthread_args
+{
+	int x_resolution;
+	int y_resolution;
+	int max_iter;
+	double view_x0;
+	double view_x1;
+	double view_y0;
+	double view_y1;
+	double x_stepsize;
+	double y_stepsize;
+	int palette_shift;
+	int num_thread;
+	int num_threads;
+	void *image;
 };
 
-//function to assign every thread's object it's respective values
-void obj_assign(struct thread_obj *args, int x_resolution, int y_resolution, int max_iter,
-	                double view_x0, double view_x1, double view_y0, double view_y1,
-	                double x_stepsize, double y_stepsize,
-	                int palette_shift;, unsigned char (*image)[x_resolution][3],
-						 int num_threads, int thread_num){
-     args->x_resolution = x_resolution;
-     args->y_resolution = y_resolution;
-     args->max_iter = max_iter;
-     args->paletter_shift = paletter_shift;
-     args->num_threads = num_threads;
-     args->view_x0 = view_x0;
-     args->view_x1 = view_x1;
-     args->view_y0 = view_y0;
-     args->view_y1 = view_y1;
-     args->x_stepsize = x_stepsize;
-     args->y_stepsize = y_stepsize;
-     args->image = (void*) image;
-     args->thread_num = thread_num;
-}
-void mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
-	                double view_x0, double view_x1, double view_y0, double view_y1,
-	                double x_stepsize, double y_stepsize,
-	                int palette_shift;, unsigned char (*image)[x_resolution][3],
-						 int num_threads) {
-	// TODO:
-	// implement your solution in this file.
-	pthread_t *threads = (pthread_t*) malloc(num_threads * sizeof(pthread_t));
-        struct thread_obj *args = (struct thread_obj*) malloc(num_threads * sizeof(struct thread_obj));
-        for(int i=0; i<num_threads; ++i){
-            obj_assign(args+i, x_resolution, y_resolution, max_iter, view_x0, view_x1,
-                       view_y0, view_y1, x_stepsize, y_stepsize, palette_shift,
-                      (void*) image, num_threads, (i+1));
-            pthread_create(&threads[i], NULL, kernel, (void*)args+i);
-        }
-        for(int i=0; i<num_threads; ++i){
-           pthread_join(threads[i], NULL);
-        }
-        free(args);
-        free(threads);        
-}
-
-void* kernel(void* arg){
-        struct pthread_obj *args = (struct pthread_obj*) arg;
-        int rows_per_thread = args->y_resolution/args->num_threads;
-        int row_start = rows_per_thread * (args->thread_num - 1);
-        int row_end = row_start + rows_per_thread;
-        if(args->num_threads == args->thread_num){
-           row_end = args->y_resolution;
-        }
-        unsigned char (image*)[args->x_resolution][3] = (unsigned char (*)[args->x_resolution][3]) args->image
-        double y;
+void* draw(void *args){
+	
+	struct pthread_args *arg = (struct pthread_args*) args;
+	int rows_per_thread = arg->y_resolution/arg->num_threads;
+    int row_start = rows_per_thread * (arg->num_thread - 1);
+    int row_end = row_start + rows_per_thread;
+    if(arg->num_threads == arg->num_thread){
+        row_end = arg->y_resolution;
+    }
+	unsigned char (*img)[arg->x_resolution][3] = (unsigned char (*)[arg->x_resolution][3]) arg->image;
+	double y;
 	double x;
 
 	complex double Z;
 	complex double C;
 
 	int k;
-
+	
 	for (int i = row_start; i < row_end; i++)
 	{
-		for (int j = 0; j < x_resolution; j++)
+		for (int j = 0; j < arg->x_resolution; j++)
 		{
-			y = view_y1 - i * y_stepsize;
-			x = view_x0 + j * x_stepsize;
+			y = arg->view_y1 - i * arg->y_stepsize;
+			x = arg->view_x0 + j * arg->x_stepsize;
 
 			Z = 0 + 0 * I;
 			C = x + y * I;
@@ -88,18 +58,69 @@ void* kernel(void* arg){
 			{
 				Z = Z * Z + C;
 				k++;
-			} while (cabs(Z) < 2 && k < max_iter);
+			} while (cabs(Z) < 2 && k < arg->max_iter);
 
-			if (k == max_iter)
+			if (k == arg->max_iter)
 			{
 				memcpy(img[i][j], "\0\0\0", 3);
 			}
 			else
 			{
-				int index = (k + palette_shift)
+				int index = (k + arg->palette_shift)
 				            % (sizeof(colors) / sizeof(colors[0]));
 				memcpy(img[i][j], colors[index], 3);
 			}
 		}
 	}
 }
+void obj_assign(struct pthread_args *args, int x_resolution, int y_resolution, int max_iter, double view_x0, double view_x1, double view_y0, double view_y1, double x_stepsize, double y_stepsize, int palette_shift, unsigned char (*image)[x_resolution][3], int num_threads, int thread_num){
+		args->x_resolution = x_resolution;
+		args->y_resolution = y_resolution;
+		args->max_iter = max_iter;
+		args->view_x0 = view_x0;
+		args->view_x1 = view_x1;
+		args->view_y0 = view_y0;
+		args->view_y1 = view_y1;
+		args->x_stepsize = x_stepsize;
+		args->y_stepsize = y_stepsize;
+		args->palette_shift = palette_shift;
+		args->image = (void*)image;
+		args->num_thread = thread_num;
+		args->num_threads = num_threads;
+}
+						 
+						 
+
+void mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
+	                double view_x0, double view_x1, double view_y0, double view_y1,
+	                double x_stepsize, double y_stepsize,
+	                int palette_shift, unsigned char (*image)[x_resolution][3],
+						 int num_threads) {
+	// TODO:
+	// implement your solution in this file.
+	
+	/**Creation of threads**/
+	pthread_t *threads = (pthread_t*) malloc (num_threads *sizeof(pthread_t));
+	
+	/**Assignment of arguments for each kernel respectively**/
+	struct pthread_args* args = (struct pthread_args*) malloc (num_threads*sizeof (struct pthread_args));
+	
+	/**Division of work for assignment to each thread**/
+	for (int i=0; i<num_threads; ++i){
+		
+		obj_assign(args+i, x_resolution, y_resolution, max_iter, view_x0, view_x1,
+                       view_y0, view_y1, x_stepsize, y_stepsize, palette_shift,
+                      (void*) image, num_threads, (i+1));
+		
+		//thread creation
+		pthread_create(&threads[i], NULL, draw, args+i);
+	}
+	
+	/**Joining Threads**/
+	for (int i=0; i<num_threads; ++i){
+		pthread_join(threads[i], NULL);
+	}
+	
+	free(threads); free(args);
+}
+
